fa5b7ac7cc289112f3aa77525e4c6f1894b90a71
Sahit Singh <sahit.singh1@gmail.com>
2022-05-22 15:06:15 +0530

    Update get_ips.py

--- get_ips.py
+++ get_ips.py
@@ -1,9 +1,14 @@
 import boto3
+from pprint import pprint
 
 client = boto3.client('ec2', region_name="ap-south-1")
 
 custom_filter = [{
-    'Name':'tag:Environment', 
-    'Values': ['DEV']}]
-    
+    'Name':'tag:Environment',
+    'Values': ['TEST']}
+]
+
 response = client.describe_instances(Filters=custom_filter)
+for instance in response["Reservations"]:
+    for _ins in instance["Instances"]:
+        pprint(_ins["PrivateIpAddress"])
